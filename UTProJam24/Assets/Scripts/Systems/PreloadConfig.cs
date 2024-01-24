604d073a85de66fb830c4356cc9f524ec55ae8b0
Timo654 <41972888+Timo654@users.noreply.github.com>
2024-01-24 18:07:46 +0200

    update unity to 2022.3.18f1

--- UTProJam24/Assets/Scripts/Systems/PreloadConfig.cs
+++ UTProJam24/Assets/Scripts/Systems/PreloadConfig.cs
@@ -5,6 +5,6 @@ public class PreloadConfig : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        if (BuildConstants.isMobile) Application.targetFrameRate = Screen.currentResolution.refreshRate;
+        if (BuildConstants.isMobile) Application.targetFrameRate = (int)Screen.currentResolution.refreshRateRatio.value;
     }
 }
