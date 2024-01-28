b9a660c9465f3016d343f02cabdd5fd24b21fd05
Timo654 <41972888+Timo654@users.noreply.github.com>
2024-01-28 12:55:09 +0200

    cutscenesss

---
+++ UTProJam24/Assets/Scripts/Cutscene/OPScript.cs
@@ -0,0 +1,87 @@
+using DG.Tweening;
+using FMODUnity;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class OPScript : MonoBehaviour
+{
+    [SerializeField] Sprite[] openingSprites;
+    [SerializeField] EventReference[] sfxRef;
+    [SerializeField] EventReference music;
+    private InputAction skipButton;
+    int currentSprite = 0;
+    public string nextScene = "GameScene";
+    bool inputBlocked = false;
+    Image _nextImage;
+    Image _image;
+    private PlayerControls playerControls;
+
+    private void OnEnable()
+    {
+        playerControls = new PlayerControls();
+        skipButton = playerControls.UI.Back;
+        skipButton.Enable();
+        skipButton.performed += OnSkipButton;
+    }
+
+    private void OnDisable()
+    {
+        skipButton.Disable();
+    }
+    public void OnSkipButton(InputAction.CallbackContext context)
+    {
+        inputBlocked = true;
+        OnOpeningEnd();
+    }
+
+    void Start()
+    {
+        _image = transform.GetChild(2).GetComponent<Image>();
+        _image.sprite = openingSprites[currentSprite];
+        _nextImage = transform.GetChild(1).GetComponent<Image>();
+        AudioManager.Instance.InitializeMusic(music);
+        if (currentSprite < sfxRef.Length)
+            RuntimeManager.PlayOneShot(sfxRef[currentSprite]);
+        LevelChanger.Instance.FadeIn();
+    }
+    void Update()
+    {
+        bool mobileTouch = false;
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == UnityEngine.TouchPhase.Began) mobileTouch = true;
+        }
+        if ((Input.anyKeyDown || mobileTouch) & !inputBlocked)
+        {
+            currentSprite += 1;
+            if (currentSprite < openingSprites.Length)
+            {
+                StartCoroutine(FadeSprites(openingSprites[currentSprite]));
+                if (currentSprite < sfxRef.Length)
+                    RuntimeManager.PlayOneShot(sfxRef[currentSprite]);
+            }
+            else
+            {
+                OnOpeningEnd();
+            }
+        }
+    }
+    public void OnOpeningEnd()
+    {
+        LevelChanger.Instance.FadeToLevel(nextScene);
+    }
+
+    IEnumerator FadeSprites(Sprite newSprite)
+    {
+        _nextImage.sprite = newSprite;
+        _image.DOFade(0.0f, 1f);
+        inputBlocked = true;
+        yield return new WaitForSecondsRealtime(1f);
+        _image.DOKill();
+        _image.sprite = newSprite;
+        _image.DOFade(1.0f, 0f);
+        inputBlocked = false;
+    }
+}
